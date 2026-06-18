b16c1e458d6ecb6ca68046970f8adf8f018caea3
Anton Smirnov <asmirnov@quantum-systems.com>
2026-06-18 12:16:30 +0000

    upd

--- init.lua
+++ init.lua
@@ -130,7 +130,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path = 2}}, -- show absolute path (0 - fname, 1 - relative, 2 - absolute)
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
